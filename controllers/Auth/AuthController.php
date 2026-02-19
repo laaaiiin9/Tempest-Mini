b3698a17e812af60e2d1117b0e3c284f235dcd2e
Synepis <allainjamesdayagdag04@gmail.com>
2026-02-20 01:19:25 +0800

    restructured some entry points to App object

--- controllers/Auth/AuthController.php
+++ controllers/Auth/AuthController.php
@@ -4,7 +4,6 @@ class AuthController extends Controller {
 
     public function index(): void {
         Response::view('auth/login', []);
-        //$this->view('auth/login', []);
     }
 
     public function login(): void {
---
+++ core/App.php
@@ -0,0 +1,53 @@
+<?php
+
+class App
+{
+
+    public Router $router;
+
+    public function run(): void
+    {
+        $config_app = require BASE_PATH . '/config/app.php';
+        $config_db = require BASE_PATH . '/config/database.php';
+
+        require BASE_PATH . '/core/Config.php';
+
+        Config::set('app', $config_app);
+        Config::set('database', $config_db);
+
+        if (Config::get('app.env') === 'local') {
+            ini_set('display_errors', 1);
+            error_reporting(E_ALL);
+        } else {
+            ini_set('display_errors', 0);
+        }
+
+        require BASE_PATH . '/core/Response.php';
+
+        set_error_handler(function ($severity, $message, $file, $line) {
+            throw new ErrorException($message, 0, $severity, $file, $line);
+        });
+
+        set_exception_handler(function ($exception) {
+
+            if (Config::get('app.env') === 'local') {
+                Response::abort(500, '<pre>' . $exception . '<pre>');
+            } else {
+                Response::abort(500, 'Server Error');
+            }
+
+            exit;
+        });
+
+        require BASE_PATH . '/db/DB.php';
+        DB::initDb();
+
+        require BASE_PATH . '/core/Router.php';
+
+        $this->router = new Router();
+
+        $router = $this->router;
+        require BASE_PATH . '/routes/web.php';
+    }
+
+}
